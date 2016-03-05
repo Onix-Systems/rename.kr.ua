7159071e84249ebd434af695ab8a0afbbf8f8815
canabina <konstantin255@gmail.com>
2016-03-05 17:09:16 +0200

    Charset encoding fix

--- app/modules/frontend/controllers/MainController.php
+++ app/modules/frontend/controllers/MainController.php
@@ -6,15 +6,15 @@ class MainController extends IndexController
 	public function actionIndex()
 	{
 		$this->pageTitle = 'Rename.kr.ua - Нові назви вулиць міста Кіровограда';
-		
+		mb_internal_encoding("UTF-8");
 		$responce = [];
 		foreach (Sili::$model->data->getData(['ORDER' => 'old_name', 'old_name[!]' => '']) as $key => $value) 
 			$responce[mb_substr($value['old_name'], 0, 1)][] = $value;
 
-
 		echo "<!--";
 		print_r($responce);
 		echo "-->";
+
 
 		$this->render = ['main', [
 									'data' => $responce,
