3a9c3be631342280f85767b090a09f0fb800336e
canabina <konstantin255@gmail.com>
2016-03-05 17:02:57 +0200

    f

--- app/modules/frontend/controllers/MainController.php
+++ app/modules/frontend/controllers/MainController.php
@@ -11,6 +11,11 @@ class MainController extends IndexController
 		foreach (Sili::$model->data->getData(['ORDER' => 'old_name', 'old_name[!]' => '']) as $key => $value) 
 			$responce[mb_substr($value['old_name'], 0, 1)][] = $value;
 
+
+		echo "<!--";
+		print_r($responce);
+		echo "-->";
+
 		$this->render = ['main', [
 									'data' => $responce,
 									'areas' => Sili::$db->select('areas', '*'),
