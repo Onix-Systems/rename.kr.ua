60a89336a20c832f1a28696b5189dd3a78d4430f
Sergey Kholin <xainSE@gmail.com>
2016-03-06 09:08:23 +0200

    Added information about designer

--- app/modules/frontend/views/about.php
+++ app/modules/frontend/views/about.php
@@ -3,12 +3,7 @@
 </h3>
 
 <div class="page-body">
-	<p>
-		<b>
-			Ідея створення
-		</b>
-	</p>
-
+	<p><b>Ідея створення</b></p>
 	<p>
 		Ідея створення сайту з'явилася після того, як в мережі потрапив в поле зору сайт <a href="http://rename.dp.ua">http://rename.dp.ua</a>. Декомунізація об’єктів в Кіровограді йде не швидко і спостерігати за цими змінами, читаючи постанови виконавчого комітету міської ради дуже не зручно. В той же самий час і картографічні сервіси не встигають відслідковувати і правильно показувати назви вулиць, чи знаходити потрібні адреси.
 	</p>
@@ -17,7 +12,10 @@
 		В прозробці сайту приняли участь
 		<a href="https://www.facebook.com/bohdan.stasiuk" target="__blank">Богдан Стасюк</a> - ідея, підготовка інформації про зміни назв, 
 		<a href="https://www.facebook.com/profile.php?id=100009344186941" target="__blank">Максим Сінченко</a> - підготовка інформації про вулиці, 
-		<a href="https://www.facebook.com/xainse" target="__blank">Холін Сергій</a> - організатор, <a href="https://www.facebook.com/profile.php?id=100000771316650" target="__blank">Константин Алтухов</a> - розробник, <a href="https://www.facebook.com/durach" target="__blank">Дмитро Дурач</a> - моральна підтримка.
+		<a href="https://www.facebook.com/xainse" target="__blank">Холін Сергій</a> - організатор, 
+		<a href="https://www.facebook.com/profile.php?id=100000771316650" target="__blank">Константин Алтухов</a> - розробник, 
+		<a href="https://www.facebook.com/profile.php?id=100005641680554" target="__blank">Константин Свідзінський</a> - дизайнер, 
+		<a href="https://www.facebook.com/durach" target="__blank">Дмитро Дурач</a> - моральна підтримка.
 	</p>
 
 	<p>
@@ -25,4 +23,5 @@
 			Скарги та пропозиції надсилайте на електронну пошту: <a mailto='sergey.kholin@onix-systems.com'>sergey.kholin@onix-systems.com</a>
 		</b>
 	</p>
+	
 </div>
