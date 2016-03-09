1f13f8d75ca69b07632055cd4b9a0f67a19222e0
canabina <konstantin255@gmail.com>
2016-03-09 14:53:09 +0200

    Updates

--- app/modules/frontend/views/layouts/error.php
+++ app/modules/frontend/views/layouts/error.php
@@ -2,20 +2,14 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>404</title>
+		<title>404 - Сторінку не знайдено</title>
 		<link rel="stylesheet" href="/assets/css/main.min.css" type='text/less'>
 	</head>
 	<body>
-	
-
-		
-
 		<div class="content-wrapper">
 			<div class="content">
-				
 				<?= $content ?>
 			</div>
 		</div>
-
 	</body>
 </html>
