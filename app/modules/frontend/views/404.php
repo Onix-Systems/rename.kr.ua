cd689fef4d3728f9bd49f77a2df6328635ef1af7
canabina <konstantin255@gmail.com>
2016-04-01 14:09:55 +0300

    Update 404

--- app/modules/frontend/views/404.php
+++ app/modules/frontend/views/404.php
@@ -1,4 +1,4 @@
 <div class="page-body error-page">
-	<h1 align='center'>Упс! Строінка "<?= $_SERVER['REQUEST_URI'] ?>" не знайдена. Але ви завжди можете перейти на <a href="/">головну</a>.</h1>
+	<h1 align='center'>Упс! Строінка "<?= Sili::$app->request->server('REQUEST_URI') ?>" не знайдена. Але ви завжди можете перейти на <a href="/">головну</a>.</h1>
 	<div align='center' style='margin-top: 30px;'><a href="/">© Rename.kr.ua</a></div>
 </div>
